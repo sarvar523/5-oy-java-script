481e050fc7bc224c4908677399ef98089a1860c0
sarvar523 <sarvarakmalovich523@gmail.com>
2025-07-17 14:08:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let text = prompt("salom")                                                  
+    let number = prompt( " familangiz")
+        let pasword = prompt( " yoqtirgan faniz")
+            let region= prompt( " nechi yoshsiz")
+
+console.log(text)
